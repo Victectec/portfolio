b849cabc7e9ce6f4b9422c8f515ec372d59cc219
Viraoz <vtaraujo2070@gmail.com>
2024-11-27 11:42:17 -0300

    header

--- app.js
+++ app.js
@@ -17,6 +17,25 @@ app.get('/curriculo', (req, res) => {
     res.sendFile(path.join(__dirname,"public", 'curriculo/curriculo.html'));
 });
 
+app.get('/cnt', (req, res) => {
+    res.sendFile(path.join(__dirname,"public", 'cnt/cnt.html'));
+});
+app.get('/chs', (req, res) => {
+    res.sendFile(path.join(__dirname,"public", 'chs/chs.html'));
+});
+
+app.get('/iot', (req, res) => {
+    res.sendFile(path.join(__dirname,"public", 'iot/iot.html'));
+});
+
+app.get('/matematica', (req, res) => {
+    res.sendFile(path.join(__dirname,"public", 'matematica/matematica.html'));
+});
+
+app.get('', (req, res) => {
+    res.sendFile(path.join(__dirname,"public", 'header.html'));
+});
+
 app.listen(port, () => {
     console.log(`Servidor rodando na porta ${port}`);
 });
